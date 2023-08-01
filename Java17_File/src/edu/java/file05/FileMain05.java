004e1fe4af7df8204999cc8b37174a027d6d3376
goott3 <goott3@DESKTOP-EVVH0B0>
2023-08-01 09:36:25 +0900

    first

---
+++ Java17_File/src/edu/java/file05/FileMain05.java
@@ -0,0 +1,54 @@
+package edu.java.file05;
+
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+
+import edu.java.file04.MemberDTO;
+
+// 프로그램 <=== ObjectInputStream <=== FileInputStream <=== 파일(HDD)
+
+public class FileMain05 {
+
+	public static void main(String[] args) {
+		InputStream in = null;
+		ObjectInputStream oin = null;
+		
+		try {
+			in = new FileInputStream("temp/member.txt");
+			oin = new ObjectInputStream(in);
+			
+			while(true) {
+				try {
+					MemberDTO m = (MemberDTO) oin.readObject();
+					System.out.println(m);					
+				} catch (EOFException e) {
+					break;
+				}
+			}
+			
+			System.out.println("데이터 출력 완료");
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		} finally {
+			try {
+				oin.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	} // main()
+
+} // end FileMain05
+
+
+
+
+
+
+
+
